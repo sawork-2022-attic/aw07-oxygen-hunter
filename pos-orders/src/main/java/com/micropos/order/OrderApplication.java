9e51c3a74e5e2d3605c1f39caed099fc703a4aac
m1rr0r <xiao.li@smail.nju.edu.cn>
2022-05-16 18:28:13 +0800

    order can send orderDto to rabbitmq

--- pos-orders/src/main/java/com/micropos/order/OrderApplication.java
+++ pos-orders/src/main/java/com/micropos/order/OrderApplication.java
@@ -1,8 +1,12 @@
 package com.micropos.order;
 
+import com.micropos.dto.OrderDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 @EnableDiscoveryClient
